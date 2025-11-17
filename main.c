24b24deb43847a24a9df3fb4498140e5072ddbc3
Vaishnavi-25-s <vaishnavi.s25@gcu.edu.in>
2025-11-17 07:51:49 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[100];
+    char branch[20];
+    printf("enter your name\n");
+    scanf("%s",&name);
+    printf("your name is %s\n", name);
+    printf("enter your branch\n");
+    scanf("%s",&branch);
+    printf("your branch is %s\n", branch);
+    return 0;
+
+
+}
